8b1ae91687e6488f03070f7cbf5de5dd8874b10c
Kalesezeta <kalesezeta@gmail.com>
2012-11-30 23:47:41 +0100

    index tpl update

--- Site/index.tpl.php
+++ Site/index.tpl.php
@@ -16,11 +16,15 @@
 	<link rel="stylesheet" type="text/css" href="css/fonts.css" />
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 	<link rel="stylesheet" type="text/css" href="css/yui-reset.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/jquery.mCustomScrollbar.css" />
 
 	<!-- SCRIPTS -->
 	<script src="js/jquery-1.8.2.min.js" type="text/javascript"></script><!-- jQuery 1.8.2 -->
-	<script src="js/blearning.utils.js" type="text/javascript"></script><!-- Site Utils -->	
+	<script src="js/jquery-ui-1.9.2.custom.min.js" type="text/javascript"></script><!-- jQuery UI 1.9.2 -->
+	<script src="js/jquery.mousewheel.min.js" type="text/javascript"></script><!-- jQuery Mouse Wheel -->
+	<script src="js/jquery.mCustomScrollbar.min.js" type="text/javascript"></script><!-- jQuery Custom Scrollbar -->
 	<script src="js/jquery.tweet.js" type="text/javascript"></script><!-- jQuery Tweet -->
+	<script src="js/blearning.utils.js" type="text/javascript"></script><!-- Site Utils -->
 
 </head><!-- HEAD -->
 
@@ -94,7 +98,6 @@
 
 				<!-- OBJETIVOS -->
 				<div class="md-section md sec-odd objetivos cf">
-
 					<div class="sec-hd">
 						<h2 class="tx-title"><em>O</em>bjetivos</h2>
 						<div class="mob-hr">
@@ -104,7 +107,6 @@
 						</div><!-- .mob-hr -->
 						<div class="mg"></div><!-- .mg -->
 					</div><!-- .sec-hd -->
-
 					<div class="sec-bd">
 						<span class="line-closer tr"></span>
 						<span class="line-closer br"></span>
@@ -112,14 +114,11 @@
 						<p class="mt">Aunque resulta imposible saber con certeza lo que deparara el futuro en esta nueva sociedad cambiante. todo parece indicar que la tendencia son los sistemas de aprendizaje mixtos: presenciales y a distancia (Blended learning o. simplemente. b-leamlngy Así. es posible que lo que hoy se denomina b-Iearning pueda llegar a ser denominado simplemente aprendizaje. El b-Ieamlng está aquí para quedarse. La razón más poderosa es su gran eficacia en la adquisición de estrategias y competencias por parte de quien aprende.</p>
 						<q class="mq">Nuestro trabajo consiste en formar expertos en la creación. gestión. diseño y dirección de proyectos de Educación a Distancia en la modalidad de learning y e-Iearning, tanto en instituciones educativas como en empresas”</q>
 					</div><!-- .sec-bd -->
-
 					<div class="sec-ft"><span class="ft-bullets"></span></div>
-
-				</div><!-- .md-section -->
+				</div><!-- .md-section.objetivos -->
 
 				<!-- DESTINATARIOS -->
 				<div class="md-section md sec-even destinatarios cf">
-
 					<div class="sec-hd">
 						<h2 class="tx-title"><em>D</em>estinatarios</h2>
 						<div class="mob-hr">
@@ -129,7 +128,6 @@
 						</div><!-- .mob-hr -->
 						<div class="mg"></div><!-- .mg -->
 					</div><!-- .sec-hd -->
-
 					<div class="sec-bd">
 						<span class="line-closer tl"></span>
 						<span class="line-closer bl"></span>
@@ -143,8 +141,7 @@
 							</div><!-- .cp-bd -->
 						</div><!-- .cp-infobox -->
 					</div><!-- .sec-bd -->
-
-				</div><!-- .md-section -->
+				</div><!-- .md-section.destinatarios -->
 
 			</div><!-- .rg-bd -->
 
@@ -155,15 +152,138 @@
 			<div class="limiter cf">
 
 				<div class="grp-col cf">
+
 					<div class="col first fl">
-						UNO
+						<div class="md-ft_module opiniones md">
+							<div class="hd">
+								<h2 class="tx-title"><em>O</em>piniones</h2>
+							</div><!-- .hd -->
+							<div class="bd">
+								<div class="reviews-cnt">
+									<div class="review-item">
+										<p class="tx-review">"Ha ampliado mis perspectivas de trabajo y significa un logro más, profesional y personalmente."</p>
+										<p class="author">
+											Laura Guzmán Suasnávar <small>Profesora de ELE. Berlín, Alemania (Curso 2011-2012)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"El descubrimiento de una nueva forma de aprender realmente productiva y divertida, así como la adquisición de unos conocimientos fundamentales para mi actual actividad profesional."</p>
+										<p class="author">
+											 Iñaki Mendinueta Garin <small>Director de Programas Formativos. (Curso 2011-2012)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Un curso estupendo, tanto de contenido como de la labor del equipo docente. Mis felicitaciones!!"</p>
+										<p class="author">
+											Pablo Barrios <small>Director de Cómputo Académico Universidad Mariano Gálvez. Guatemala (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"El curso ha llenado al 100% y aún excedido mis expectativas. Me habría gustado algunos módulos fueran más extensos. para un mayor enriquecimiento. ¡Perfecto!"</p>
+										<p class="author">
+											Katia Zlotnik <small>Gerente, INNOVA BETA FORMACION (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Un giro muy positivo en mi formación docente y el hallazgo de una comunidad en la que el intercambio generoso de reflexiones, experiencias y la calidad humana han sido en todo momento lo más gratificante."</p>
+										<p class="author">
+											Felipe J. V. <small>(Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Comparándome con los exploradores del nuevo mundo del siglo XV, me siento un explorador del nuevo mundo pero con mapas y cartas de navegación actualizadas. Gracias por ayudarme a descubrir el mundo del blearning con la confianza de que ahora realmente se de que va el tema."</p>
+										<p class="author">
+											Jose Feliciano Garrido <small>Gerente Academia MINERVA de Fuenlabrada. Madrid (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Una experiencia muy gratificante y enriquecedora."</p>
+										<p class="author">
+											Pepi Rodríguez <small>(Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"La dinámica de todo el curso (tomando en cuenta perfil de mis compañeros y docentes) me generó un gran reto que me motivo a aprender. Verdaderamente una experiencia que cambio mi pensamiento y me hizo sentir cercanía emocional aun con tanta lejanía física."</p>
+										<p class="author">
+											Ligia <small>(Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Este curso me ha ayudado a tener una visión diferente de la docencia a la que estaba acostumbrada, espero en un futuro poder desarrollar laboralmente todo lo aprendido y así poder profundizar más. Ha sido todo un placer!!! Gracias!!!"</p>
+										<p class="author">
+											Patricia García <small>Docente. Valencia (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Ha supuesto una oportunidad para formarme en un ámbito del que conocía poco, un impulso a mi carrera profesional en momentos bastante difíciles de mi vida."</p>
+										<p class="author">
+											Alfonso Lerma Gallego <small>Formador autónomo. Sevilla (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Buen curso. Hay dedicarle horas. Provechoso."</p>
+										<p class="author">
+											Silvia Loarce <small>Responsable de formación (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"El curso ha superado con creces mis expectativas. Han sido unos meses de mucho estudio, en los que he tenido la oportunidad de aprender cuestiones completamente novedosas para mí sobre el b-learning y las implicaciones de su implementación en las empresas y centros educativos. Estoy contenta por haber hecho este curso y animo y recomiendo a otras personas interesadas en el b-learning a hacer el curso."</p>
+										<p class="author">
+											Tatiana Ribeiro <small>Profesora de portugués como lengua extranjera en academias de Madrid. (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Una intensa experiencia personal en la que se desarrollan muchas habilidades y una valiosísima y exigente formación en un área de rabiosa actualidad propiciado por el auge de las nuevas tecnologías de la información y comunicación."</p>
+										<p class="author">
+											Mª Angeles Martín Aguilar <small>Empresaria (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Excelente panorámica del b-learning. Echo en falta, eso sí, más formación relacionada con el diseño gráfico porque el interface puede invitarnos o hacernos abandonar un curso. También me hubiera gustado recibir conocimientos de marketing para comercializar los productos formativos realizados por nosotros. Creo que hubiera completado el magnífico curso que hemos recibido."</p>
+										<p class="author">
+											Isabel Andrade <small>Coordinadora proyectos online. Madrid (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Es la parte que me faltaba para emprender la realización de cursos b-Learning, ahora se que tenía que hacerlo."</p>
+										<p class="author">
+											Francisco José Espinosa Pérez <small>Técnico de televisión (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<div class="review-item">
+										<p class="tx-review">"Este curso ha representado una puerta abierta para conocer y aplicar los principios fundamentales del b-learninig en diferentes ámbitos de actuación. Ha sido una experiencia maravillosa y motivante que nos ha enseñado a gestionar y emprender una nueva modalidad de educación con calidad."</p>
+										<p class="author">
+											 Evelin Rojas Villarroel <small>Profesora Titular de Biquímica. Facultad de Veterinaria, Universidad del Zulia. Venezuela (Curso 2010-2011)</small>
+										</p><!-- .author -->
+									</div><!-- .review-item -->
+									<p class="cp-btn_line tx-ct">
+										<a class="btn bt-sk1" href="http://www.uned.es/blearning/estadisticas/" title="Ver más opiniones">Ver más opiniones</a>
+									</p><!-- .cp-btn_line -->
+								</div><!-- .reviews-cnt -->
+							</div><!-- .bd -->
+						</div><!-- .md-ft_module.opiniones -->
 					</div><!-- .col.first -->
+
 					<div class="col fl">
-						DOS
+						<div class="md-ft_module ultimos_tweets md">
+							<div class="hd">
+								<h2 class="tx-title"><em>Ú</em>ltimos tweets</h2>
+							</div><!-- .hd -->
+							<div class="bd">
+							</div><!-- .bd -->
+						</div><!-- .md-ft_module.ultimos_tweets -->
 					</div><!-- .col -->
+
 					<div class="col last fl">
-						TRES
+						<div class="md-ft_module contacto md">
+							<div class="hd">
+								<h2 class="tx-title"><em>C</em>ontacto</h2>
+							</div><!-- .hd -->
+							<div class="bd">
+							</div><!-- .bd -->
+						</div><!-- .md-ft_module.contacto -->
 					</div><!-- .col -->
+
 				</div><!-- .grp-col -->
 
 			</div><!-- .limiter -->
