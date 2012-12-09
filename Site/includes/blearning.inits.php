867f152075991fc8c0896b1a4b27bc61fd9bb930
Kalesezeta <kalesezeta@gmail.com>
2012-12-09 04:16:27 +0100

    site inits update

--- Site/includes/blearning.inits.php
+++ Site/includes/blearning.inits.php
@@ -2,21 +2,8 @@
 
 	jQuery(document).ready(function() {
 
-		// Content limiter dinamic margin
-		var navbarWidth = jQuery("#navbar").width();
-		var navbarHeight = jQuery("#navbar").height();
-
-		if (navbarWidth <= 400) {
-			jQuery('#limiter-cnt').css('margin-top', Number(navbarHeight+34) + 'px');
-		}
-		else {
-			jQuery('#limiter-cnt').css('margin-top', Number(navbarHeight+32) + 'px');
-		}
-
-		jQuery(window).resize(function() {
-				navbarHeight = jQuery("#navbar").height();
-				jQuery('#limiter-cnt').css('margin-top', Number(navbarHeight+32) + 'px');
-		});
+		BL.siteContentMargin();
+		BL.tweetsDisplayer();
 
 	});
 
@@ -28,16 +15,6 @@
 			}
 		});
 
-		jQuery(function() {
-			jQuery("#latestTweets").tweet({
-				username: "blearningUNED",
-				count: 4,
-				loading_text: "Cargando tweets..."
-			});
-		});
-
 	});
-
-	/* BL.foo("bar"); */
 
 </script>
