389213890419ce34ea91833e3da333a96adbe6e6
Kalesezeta <kalesezeta@gmail.com>
2013-04-09 21:24:38 +0200

    sec-inscripcion & contact_seccess udded

---
+++ Site/includes/contact_success.tpl.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+
+<head><!-- HEAD -->
+
+	<!-- TITLE -->
+	<title>UNED B-learning - Curso de Experto 5ª edición (2012/2013)</title>
+
+	<!-- METAS -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="UNED B-learning - Curso de Experto 5ª edición (2012/2013)" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- STYLE SHEETS -->
+	<link rel="stylesheet" type="text/css" href="../css/bl_fonts.css" />
+	<link rel="stylesheet" type="text/css" href="../css/bl_styles.css" />
+	<link rel="stylesheet" type="text/css" href="../css/yui-reset.min.css" />
+	<link rel="stylesheet" type="text/css" href="../css/jquery.qtip.css" />
+	<link rel="stylesheet" type="text/css" href="../css/colorbox.css" />
+	<link rel="stylesheet" type="text/css" href="../css/jquery.mCustomScrollbar.css" />
+
+	<!-- SCRIPTS -->
+	<script src="../js/jquery-1.8.2.min.js" type="text/javascript"></script><!-- jQuery 1.8.2 -->
+	<script src="../js/jquery-ui-1.9.2.custom.min.js" type="text/javascript"></script><!-- jQuery UI 1.9.2 -->
+	<script src="../js/jquery.mousewheel.min.js" type="text/javascript"></script><!-- jQuery Mouse Wheel -->
+	<script src="../js/jquery.scrollTo-1.4.3.1-min.js" type="text/javascript"></script><!-- jQuery ScrollTo 1.4.3.1 -->
+	<script src="../js/jquery.qtip.min.js" type="text/javascript"></script><!-- jQuery qTip2 -->
+	<script src="../js/jquery.colorbox-min.js" type="text/javascript"></script><!-- jQuery ColorBox -->
+	<script src="../js/jquery.trunk8.min.js" type="text/javascript"></script><!-- jQuery Trunk8 -->
+	<script src="../js/jquery.mCustomScrollbar.min.js" type="text/javascript"></script><!-- jQuery Custom Scrollbar -->
+	<script src="../js/jquery.tweet.js" type="text/javascript"></script><!-- jQuery Tweet -->
+	<script src="../js/jquery.form-validation-and-hints.js" type="text/javascript"></script><!-- jQuery Form Validation & Hints -->
+	<script src="../js/blearning.utils.js" type="text/javascript"></script><!-- Site Utils -->
+
+</head><!-- HEAD -->
+
+
+<body class=""><!-- BODY --><!-- .st-debug -->
+
+	<div id="main">
+
+		<div id="wrp-debug"></div><!-- #wrapper-debug -->
+
+		<div id="navbar">
+
+			<div id="wrp-navbar" class="tx-ct">
+
+			</div><!-- .wrp-navbar -->
+
+		</div><!-- #navbar -->
+
+		<div id="limiter-cnt">
+
+			<div id="rg-hd">
+
+				<h1 class="st-brand" title="UNED - B-Learning">
+					<span>UNED - B-Learning</span>
+				</h1><!-- .st-brand -->
+
+				<p class="organized-by">
+					<span class="org-title">Organizados por:</span>
+					<a href="http://www.uned.es" class="org-uned" title="UNED" target="_blank"><em>UNED</em></a>
+					<a href="http://www.fundacion.uned.es" class="org-foundation" title="Fundación Uned" target="_blank"><em>Fundación Uned</em></a>
+				</p><!-- .org-by-->
+
+				<ul class="lst-social-links">
+					<li class="lst-item lnk-fb fl">
+						<a href="http://www.facebook.com/pages/Curso-de-b-learning-de-la-UNED/111111225576352" title="Facebook" target="_blank"><em>Facebook</em><span class="ico"></span><span class="stick"></span></a>
+					</li><!-- .lst-item -->
+					<li class="lst-item lnk-tw fl">
+						<a href="https://twitter.com/blearningUNED" title="Twitter" target="_blank"><em>Twitter</em><span class="ico"></span><span class="stick"></span></a>
+					</li><!-- .lst-item -->
+					<li class="lst-item lnk-contact fl">
+						<a href="#contacto" title="Contacto"><em>Contacto</em><span class="ico"></span><span class="stick"></span></a>
+					</li><!-- .lst-item -->
+				</ul><!-- .lst-social-links -->
+
+			</div><!-- .rg-hd -->
+
+			<div id="rg-bd">
+
+				<div class="md-st-title md">
+					<h1>Cursos de Experto 6ª edición <em>(2013/2014)</em></h1>
+					<p class="first-item">Experto Universitario en Diseño, gestión y dirección de proyectos de b-learning</p>
+					<p>Experto Profesional en Diseño y gestión de cursos de e-learning y b-learning</p>
+				</div><!-- .md-st-title -->
+
+				<!-- CONTACT SUCCESS -->
+				<div class="md-section md-sponsors md cf">
+					<div class="sec-bd bd" style="float:none; width:75%; margin:0 auto;">
+						<div class="logos-cnt" style="padding:15px; text-align:center;">
+							<q class="mq">
+								<span style="display:block; margin-bottom:10px;">Gracias por contactar con nosotros. Nos pondremos en contacto con usted...</span>
+								<a href="../index.tpl.php" style="font-size:18px;">Ir a los cursos</a>
+							</q>
+						</div><!-- .logos-cnt -->
+					</div><!-- .bd -->
+				</div><!-- .md-sponsors -->
+
+			</div><!-- .rg-bd -->
+
+		</div><!-- #limiter-cnt -->
+
+		<div id="rg-ft">
+
+			<div class="limiter cf">
+
+				<div class="grp-col cf">
+
+					<div class="col first">
+						<div class="md-ft_module opiniones md">
+							<div class="hd">
+								<h2 class="tx-title"><em>O</em>piniones</h2>
+							</div><!-- .hd -->
+							<div class="bd">
+								<div class="reviews-cnt">
+									<?php include('md-ft_module_opiniones-content.tpl.php'); ?>
+									<p class="cp-btn_line tx-ct">
+										<a class="btn sk-bt2" href="http://www.uned.es/blearning/estadisticas/" title="Ver más opiniones" target="_blank">Ver más opiniones</a>
+									</p><!-- .cp-btn_line -->
+								</div><!-- .reviews-cnt -->
+							</div><!-- .bd -->
+						</div><!-- .md-ft_module.opiniones -->
+					</div><!-- .col.first -->
+
+					<div class="col">
+						<div class="md-ft_module ultimos_tweets md">
+							<div class="hd">
+								<h2 class="tx-title"><em>Ú</em>ltimos Tweets</h2>
+							</div><!-- .hd -->
+							<div class="bd">
+								<span class="goto-tw"><a href="https://twitter.com/blearningUNED" target="_blank"><em>B-learning Twitter</em></a></span>
+							    <div id="latestTweets" class="query"></div>
+							</div><!-- .bd -->
+						</div><!-- .md-ft_module.ultimos_tweets -->
+					</div><!-- .col -->
+
+					<div class="col last">
+						<div class="md-ft_module contacto md" id="contacto">
+							<div class="hd">
+								<h2 class="tx-title"><em>C</em>ontacto</h2>
+							</div><!-- .hd -->
+							<div class="bd">
+								<form method="post" action="contactform.php" id="contactForm">
+									<fieldset>
+										<div class="field required">
+											<p>
+												<label>Required field</label>
+												<input type="text" name="name" class="text verifyText" title="*Nombre" id="ctform-name" />
+												<span class="iferror">Nombre requerido</span>
+											</p>
+										</div><!-- .field -->
+										<div class="field required">
+											<p>
+												<label>Email</label>
+												<input type="text" name="email" class="text verifyMail" title="*E-mail:" id="ctform-email" />
+												<span class="iferror">E-mail inválido</span>
+											</p>
+										</div><!-- .field -->
+										<div class="field required">
+											<p>
+												<label>Mensaje</label>
+												<textarea name="message" class="tx-message" title="*Mensaje:" id="ctform-message"></textarea>
+												<span class="iferror">Escriba su mensaje</span>
+											</p>
+										</div><!-- .field -->
+										<div class="cp-btn_line tx-rt">
+											<input type="submit" name="submit" value="Enviar" class="btn sk-bt2" />
+										</div><!-- .cp-btn_line -->
+									</fieldset>
+								</form>
+							</div><!-- .bd -->
+						</div><!-- .md-ft_module.contacto -->
+					</div><!-- .col.last -->
+
+				</div><!-- .grp-col -->
+
+				<div class="copyr tx-ct">
+					<div class="wrp-copyr">
+						<p class="ft-brand cf"><span class="bl-logo fl"><em>B-learning</em></span> <span class="uned-copyr fl">© UNED, 2013.</span></p>
+						<p class="tx-copyr">Cursos de Experto en b-learning y e-learning - Telf. 645 732 711 (lunes a viernes de 10:00h. a 20:00h.)</p>
+					</div><!-- .wrp-copyr -->
+				</div><!-- .copyr -->
+
+			</div><!-- .limiter -->
+
+		</div><!-- .rg-ft -->
+
+	</div><!-- #main -->
+
+<?php include('blearning.inits.php'); ?><!-- Site Inits -->
+
+</body><!-- BODY -->
+
+</html>
